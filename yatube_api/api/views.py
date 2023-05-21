b0ed4e11c34b8986d4563f25e171086f39ba7b85
Кристина Пачи <kristina.pachi.04@gmail.com>
2023-05-22 00:08:03 +0300

    Add docstring

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, permissions, filters
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions, filters, mixins
 from rest_framework.pagination import LimitOffsetPagination
 
-from posts.models import Post, Group, Follow
+from posts.models import Post, Group
 from .serializers import (
     PostSerializer,
     GroupSerializer,
@@ -12,6 +13,12 @@ from .permissions import IsAuthorOrReadOnlyPermission
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    """
+    Работает с эндпоинтами для модели Post,
+    обрабатывает запросы к API,
+    поддерживает все CRUD-операции.
+    """
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnlyPermission,)
@@ -24,36 +31,51 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Работает с эндпоинтами для модели Group,
+    обрабатывает только GET запросы к API.
+    """
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Работает с эндпоинтами для модели Comment,
+    обрабатывает запросы к API,
+    поддерживает все CRUD-операции.
+    """
+
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnlyPermission,)
 
     def get_queryset(self):
-        post = Post.objects.get(pk=self.kwargs.get('post_id'))
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         return post.comments.all()
 
     def perform_create(self, serializer):
-        post = Post.objects.get(pk=self.kwargs.get('post_id'))
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         serializer.save(
             author=self.request.user,
             post=post,
         )
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """
+    Работает с эндпоинтом для модели Follow,
+    обрабатывает GET и POST запросы к API.
+    """
+
     serializer_class = FollowSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('following__username', 'user__username',)
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
-        user = self.request.user
-        new_queryset = Follow.objects.filter(user=user)
-        return new_queryset
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user,)
